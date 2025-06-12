ea696649e3af71a8a4c68f5eef9acd54fdc7b39e
n0m4dz <tseegii.it@gmail.com>
2025-06-12 18:12:51 +0800

    Optimized backup

--- src/Puzzle/DBBackup.php
+++ src/Puzzle/DBBackup.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace Lambda\Puzzle;
+
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\File;
 
 class DBBackup
 {
@@ -12,6 +14,11 @@ class DBBackup
             return false;
         }
 
+        $path = storage_path('backup');
+        if (!File::exists($path)) {
+            File::makeDirectory($path, 0755, true);
+        }
+
         $db = config('database.connections.mysql.database');
         $user = config('database.connections.mysql.username');
         $pass = config('database.connections.mysql.password');
@@ -19,7 +26,7 @@ class DBBackup
         $exe = config('lambda.backup.exe_path');
 
         $timestamp = now()->format('Y-m-d_H-i-s');
-        $backupFile = storage_path("app/backup_{$db}_{$timestamp}.sql");
+        $backupFile = storage_path("backup/backup_{$db}_{$timestamp}.sql");
         Log::info("Creating MySQL dump...");
 
         $dumpCommand = "{$exe} --single-transaction -h {$host} -u {$user} -p'{$pass}' {$db} > {$backupFile} 2>&1";
@@ -32,6 +39,7 @@ class DBBackup
         }
 
         Log::info("Backup created at {$backupFile}");
+        $this->deleteOldBackups(storage_path('backup'), 3);
         return $backupFile;
     }
 
@@ -79,12 +87,24 @@ class DBBackup
 
         exec("expect $scriptPath 2>&1", $output, $returnCode);
 
-//        unlink($tmpPath); // delete script
+        unlink($scriptPath); // delete script
 
         return [
             'success' => $returnCode === 0,
             'output' => $output,
             'code' => $returnCode
         ];
+    }
+
+    function deleteOldBackups($path = 'storage/backup', $days = 3)
+    {
+        $files = File::files($path);
+        $now = now();
+
+        foreach ($files as $file) {
+            if ($now->diffInDays(\Carbon\Carbon::createFromTimestamp(File::lastModified($file))) > $days) {
+                File::delete($file);
+            }
+        }
     }
 }
